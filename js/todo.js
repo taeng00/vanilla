791dc89fb5051f482922d91f185f2590eac4b610
tae <kth4111502@naver.com>
2023-03-27 05:49:34 +0900

    7.1 add

--- js/todo.js
+++ js/todo.js
@@ -2,10 +2,19 @@ const toDOForm = document.getElementById("todo-form");
 const toDOInput = document.querySelector("#todo-form input"); 
 const toDOList = document.getElementById("todo-list");
 
+function paintToDo(newTodo){
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    li.appendChild(span);
+    span.innerText = newTodo;
+    toDOList.appendChild(li);
+}
+
 function handleToDoSubmit(event) {
     event.preventDefault();
     const newTodo = toDOInput.value;
     toDOInput.value = "";
+    paintToDo(newTodo);
 }
 
 toDOForm.addEventListener("submit",handleToDoSubmit);
