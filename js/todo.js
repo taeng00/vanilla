bc152e57d5625a23d1602e50fc3241d7aa3a84c0
tae <kth4111502@naver.com>
2023-03-27 06:37:40 +0900

    7.6 deleting

--- js/todo.js
+++ js/todo.js
@@ -1,44 +1,55 @@
 const toDOForm = document.getElementById("todo-form");
-const toDOInput = toDOForm.querySelector("input"); //document.querySelector("#todo-form input")
+const toDOInput = document.querySelector("#todo-form input"); 
 const toDOList = document.getElementById("todo-list");
 
-const TODOS_KEY="todos";
+
+const TODOS_KEY = "todos";
+
 let toDos = [];
+
 function savetoDos(){
-    localStorage.setItem(TODOS_KEY,JSON.stringify(toDos)); //JSON stringify는 문자열로 저장을 해주는 기능을 가짐
+    localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
 }
+
+function deleteToDo(event){
+    const li =event.target.parentElement;
+    console.log(li.id);
+    li.remove();
+}
+
 function paintToDo(newTodo){
     const li = document.createElement("li");
+    li.id =newTodo.id;
     const span = document.createElement("span");
+    span.innerText = newTodo.text;
     const button = document.createElement("button");
     button.innerText = "X";
     button.addEventListener("click",deleteToDo);
     li.appendChild(span);
-    span.innerText = newTodo;
-    toDOList.appendChild(li);
-    //console.log(li);
     li.appendChild(button);
+    toDOList.appendChild(li);
 }
-function deleteToDo(event){
-const li =event.target.parentElement;
-li.remove();
-}
+
 function handleToDoSubmit(event) {
     event.preventDefault();
     const newTodo = toDOInput.value;
     console.log(toDOInput.value);
     toDOInput.value = "";
-    toDos.push(newTodo);
-    //console.log(newTodo,toDOInput.value);
-    paintToDo(newTodo);
+    const newTodoObj ={
+        text:newTodo,
+        id: Date.now()
+    }
+    toDos.push(newTodoObj);
+    paintToDo(newTodoObj);
     savetoDos();
 }
 
 toDOForm.addEventListener("submit",handleToDoSubmit);
+
 const savedToDos = localStorage.getItem(TODOS_KEY);
-if(savedToDos !== null){
+
+if(savedToDos !== null) {
     const parsedToDos = JSON.parse(savedToDos);
-    toDos = parsedToDos;
-    //parsedToDos.forEach(sayHello);// parsedToDos.foreach(item) => console.log("this is the turn of", item) 과 같음
+    console.log(parsedToDos);
     parsedToDos.forEach(paintToDo);
-};
+}
