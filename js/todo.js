48718dfba3908bc3f4ac3b78fab60511da05503c
tae <kth4111502@naver.com>
2023-03-27 06:15:03 +0900

    7.4 loading

--- js/todo.js
+++ js/todo.js
@@ -2,9 +2,12 @@ const toDOForm = document.getElementById("todo-form");
 const toDOInput = document.querySelector("#todo-form input"); 
 const toDOList = document.getElementById("todo-list");
 
+
+const TODOS_KEY="todos";
+
 const toDos = [];
 function savetoDos(){
-    localStorage.setItem("todos",toDos);
+    localStorage.setItem(TODOS_KEY,JSON.stringify(toDos));
 }
 
 function deleteToDo(event){
@@ -34,3 +37,15 @@ function handleToDoSubmit(event) {
 }
 
 toDOForm.addEventListener("submit",handleToDoSubmit);
+
+function sayHello(item){
+    console.log("this is the turn of", item);
+}
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+
+if(savedToDos !== null){
+    const parsedToDos = JSON.parse(savedToDos);
+    console.log(parsedToDos);
+    parsedToDos.forEach(sayHello);
+};
