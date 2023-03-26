827bc17a97ddf51c6cba6a49434a46e2197c333f
tae <kth4111502@naver.com>
2023-03-27 06:30:55 +0900

    7.5 loading2

--- js/todo.js
+++ js/todo.js
@@ -1,51 +1,44 @@
 const toDOForm = document.getElementById("todo-form");
-const toDOInput = document.querySelector("#todo-form input"); 
+const toDOInput = toDOForm.querySelector("input"); //document.querySelector("#todo-form input")
 const toDOList = document.getElementById("todo-list");
 
-
 const TODOS_KEY="todos";
-
-const toDos = [];
+let toDos = [];
 function savetoDos(){
-    localStorage.setItem(TODOS_KEY,JSON.stringify(toDos));
+    localStorage.setItem(TODOS_KEY,JSON.stringify(toDos)); //JSON stringify는 문자열로 저장을 해주는 기능을 가짐
 }
-
-function deleteToDo(event){
-    const li =event.target.parentElement;
-    li.remove();
-}
-
 function paintToDo(newTodo){
     const li = document.createElement("li");
     const span = document.createElement("span");
-    span.innerText = newTodo;
     const button = document.createElement("button");
     button.innerText = "X";
     button.addEventListener("click",deleteToDo);
     li.appendChild(span);
-    li.appendChild(button);
+    span.innerText = newTodo;
     toDOList.appendChild(li);
+    //console.log(li);
+    li.appendChild(button);
 }
-
+function deleteToDo(event){
+const li =event.target.parentElement;
+li.remove();
+}
 function handleToDoSubmit(event) {
     event.preventDefault();
     const newTodo = toDOInput.value;
+    console.log(toDOInput.value);
     toDOInput.value = "";
     toDos.push(newTodo);
+    //console.log(newTodo,toDOInput.value);
     paintToDo(newTodo);
     savetoDos();
 }
 
 toDOForm.addEventListener("submit",handleToDoSubmit);
-
-function sayHello(item){
-    console.log("this is the turn of", item);
-}
-
 const savedToDos = localStorage.getItem(TODOS_KEY);
-
 if(savedToDos !== null){
     const parsedToDos = JSON.parse(savedToDos);
-    console.log(parsedToDos);
-    parsedToDos.forEach(sayHello);
+    toDos = parsedToDos;
+    //parsedToDos.forEach(sayHello);// parsedToDos.foreach(item) => console.log("this is the turn of", item) 과 같음
+    parsedToDos.forEach(paintToDo);
 };
